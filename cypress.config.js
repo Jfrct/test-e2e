e13f803fae6e141c21922f9af035bc16b2180a7d
Felipe RC <cestaro70@gmail.com>
2023-09-17 20:00:26 -0300

    Aula 27: Inicio integração

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
+  e2e: { chromeWebSecurity: false,
     baseUrl: 'https://notes-serverless-app.com',
+    viewportWidthBreakpoint: 768,
     defaultCommandTimeout: 20000,
     setupNodeEvents() {
       // implement node event listeners here
